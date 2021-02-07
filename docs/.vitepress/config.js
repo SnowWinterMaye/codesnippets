0a6dd74419076a46a22bb3375608bd10f3cc3f1c
陈雪冬 <xuedong.chen@xiaobaoonline.com>
2021-02-07 18:40:41 +0800

    初始化暂存

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  lang: "en-US",
+  title: "Snippet",
+  enhanceApp({ app, router, siteData }) {
+
+  },
+  themeConfig: {
+    nav: [
+      {
+        text: "首页",
+        link: "index",
+      },
+    ],
+
+    prevLink: true,
+    nextLink: true,
+  },
+};
